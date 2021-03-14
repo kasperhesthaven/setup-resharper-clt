6dcaa01c33afa1f7ea4fae4599118c749bf6d828
Kasper Hesthaven <kasper.hesthaven@dentsuaegis.com>
2021-03-14 23:56:03 +0100

    build: Add linting configs

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	parser: 'typescript',
+	printWidth: 80,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'none',
+	useTabs: true
+};
